270a04b7131769f78ff7e8c4cb7dd88baad5d6db
Amit Chauhan <connect2amitu@gmail.com>
2022-04-18 23:03:50 +0530

    7 line added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 console.log('hello world 1=>');
 console.log('hello world 2=>');
 console.log('hello world 3=>');
+console.log('hello world 4=>');
+console.log('hello world 5=>');
+console.log('hello world 6=>');
+console.log('hello world 7=>');
